47e23d35bec010b70292fe5bb6bff74a4f83bce5
jaxo <jaxo4life@gmail.com>
2025-11-24 23:39:48 +0800

    v1.3

--- background.js
+++ background.js
@@ -1,3 +1,15 @@
+let siteSettingsCache = {};
+
+chrome.storage.sync.get({ siteSettings: {} }, ({ siteSettings }) => {
+  siteSettingsCache = siteSettings;
+});
+
+chrome.storage.onChanged.addListener((changes) => {
+  if (changes.siteSettings) {
+    siteSettingsCache = changes.siteSettings.newValue;
+  }
+});
+
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
     id: "toggle-site",
@@ -12,24 +24,21 @@ chrome.tabs.onUpdated.addListener(updateMenuTitle);
 function updateMenuTitle() {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     if (!tabs.length) return;
-    const tab = tabs[0];
 
+    const tab = tabs[0];
     if (!tab.url) return;
 
     const site = new URL(tab.url).origin;
 
-    chrome.storage.sync.get({ siteSettings: {} }, ({ siteSettings }) => {
-      if (siteSettings[site] === undefined) {
-        siteSettings[site] = true;
-        chrome.storage.sync.set({ siteSettings });
-      }
-      const disabled = siteSettings[site] === false;
+    if (siteSettingsCache[site] === undefined) {
+      siteSettingsCache[site] = true;
+      chrome.storage.sync.set({ siteSettings: siteSettingsCache });
+    }
 
-      chrome.contextMenus.update("toggle-site", {
-        title: disabled
-          ? "Enable EFP on this site"
-          : "Disable EFP on this site",
-      });
+    const disabled = siteSettingsCache[site] === false;
+
+    chrome.contextMenus.update("toggle-site", {
+      title: disabled ? "Enable EFP on this site" : "Disable EFP on this site",
     });
   });
 }
@@ -38,18 +47,16 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId !== "toggle-site") return;
 
   const site = new URL(tab.url).origin;
+  const disabled = siteSettingsCache[site] === false;
 
-  chrome.storage.sync.get({ siteSettings: {} }, ({ siteSettings }) => {
-    const disabled = siteSettings[site] === false;
-    siteSettings[site] = disabled ? true : false;
+  siteSettingsCache[site] = disabled ? true : false;
 
-    chrome.storage.sync.set({ siteSettings }, () => {
-      chrome.tabs.reload(tab.id);
-    });
+  chrome.storage.sync.set({ siteSettings: siteSettingsCache }, () => {
+    chrome.tabs.reload(tab.id);
+  });
 
-    chrome.tabs.sendMessage(tab.id, {
-      type: "SHOW_BLOCK_MESSAGE",
-      text: disabled ? "EFP Enabled on this site" : "EFP Disabled on this site",
-    });
+  chrome.tabs.sendMessage(tab.id, {
+    type: "SHOW_BLOCK_MESSAGE",
+    text: disabled ? "EFP Enabled on this site" : "EFP Disabled on this site",
   });
 });
